5928af6039cd439a3ac858599da4be5d86c84d0f
natebermudez <natebermudez01@gmail.com>
2025-04-14 23:51:00 -0400

    Added more functionality setup

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function loadPlaylist(playlistID) {
     const player = document.getElementById('player');
     player.src = `https://www.youtube.com/embed/videoseries?list=${playlistID}&autoplay=1`;
 }
+
+function changeColorMode(color) {
+    document.body.style.backgroundColor = color;
+}
