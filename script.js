dc9497fe16aade41a4a659f47cb600a81c8f4761
natebermudez <natebermudez01@gmail.com>
2025-04-29 23:44:49 -0400

    Added more playlists and password functionality

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ function loadPlaylist(playlistID) {
     player.src = `https://www.youtube.com/embed/videoseries?list=${playlistID}&autoplay=1`;
 }
 
-function changeColorMode(color) {
-    document.body.style.backgroundColor = color;
-}
+
+document.getElementById('password').addEventListener('input', function() {
+    const password = this.value;
+    const correctPassword = "horizons";
+
+    if (password === correctPassword) {
+        document.getElementById('gameDevSection').style.display = 'block';    
+        document.getElementById('codingSection').style.display = 'block';
+        document.getElementById('password').style.display = 'none';
+    } else {
+        document.getElementById('gameDevSection').style.display = 'none';
+        document.getElementById('codingSection').style.display = 'none';
+    }
+});
